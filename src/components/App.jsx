1e9b7ebbf33537179129a75468925cd3edb3af49
imevinle@gmail.com <imevinle@gmail.com>
2023-05-28 22:09:41 +0800

    feat: added clear button + row check + styling + l + ratio

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -54,7 +54,8 @@ const NUMBER_OF_CHOICES = 6;
 
 const App = () => {
   const [numArray, setNumArray] = React.useState(
-    Array(NUMBER_OF_CHOICES).fill(0)
+    // Array(NUMBER_OF_CHOICES).fill(0)
+    [1, 2, 3, 4, 5, 6]
   );
 
   const [possibleNumbers] = React.useState(() => {
@@ -65,29 +66,37 @@ const App = () => {
     return temp;
   });
 
+  const classes = {
+    unselectedMode: "bg-white text-black border-black opacity-40",
+    selectedMode: "bg-black text-white",
+  };
+
   const [currMode, setMode] = React.useState(MODES[0]);
 
   const [series, setSeries] = React.useState([]);
 
   const getSeries = () => {
-    return (currMode.code == "perm"
+    return currMode.code == "perm"
       ? getPermutations(numArray, NUMBER_OF_CHOICES)
-      : getCombinations(numArray, NUMBER_OF_CHOICES));
+      : getCombinations(numArray, NUMBER_OF_CHOICES);
   };
 
   return (
     <div>
-      <div className="bg-black p-3 mb-3 flex items-center justify-evenly ">
+      <div className="mb-3 flex items-center justify-evenly">
         {MODES.map((mode) => {
           return (
             <button
               key={mode.code}
-              className="border-r-white text-white px-5 py-2 rounded-sm hover:text-opacity-70 flex-grow"
+              className={`border-r-white px-5 py-2 rounded-sm hover:text-opacity-70 flex-grow h-max w-max bg-black ${
+                mode == currMode ? classes.selectedMode : classes.unselectedMode
+              } h-15`}
               onClick={() => {
                 setMode(mode);
               }}
             >
               {mode.label}
+
             </button>
           );
         })}
@@ -147,20 +156,74 @@ const App = () => {
           +
         </button>
       </div>
-      <h1>Total Combinations: {series.length}</h1>
-      <button
-        className="rounded bg-red-800 text-white px-5 py-2 text-center"
-        onClick={() => setSeries(getSeries())}
-      >
-        CALCULATE{" "}
-      </button>
-      <ul>
-        {series.map((n) => (
-          <li key={n}>{JSON.stringify(n)}</li>
-        ))}
-      </ul>
+      <div className="flex justify-center items-center flex-col">
+        <h1 className="m-5 text-xl">Total Combinations: {series.length}</h1>
+        <button
+          className="rounded bg-red-800 text-white px-5 py-2 text-center m-5"
+          onClick={() => setSeries(getSeries())}
+        >
+          CALCULATE{" "}
+        </button>
+        <button onClick={() => setSeries(() => [])}>CLEAR</button>
+        <ul className="w-full flex flex-col justify-center items-center gap-10 my-5 transition-all">
+          {series.map((n, index) => {
+            return (
+              <CombinationRow
+                n={n}
+                index={index}
+                removeSelf={() =>
+                  setSeries((arr) => {
+                    let temp = [...arr];
+                    temp.splice(index, 1);
+                    return temp;
+                  })
+                }
+              />
+            );
+          })}
+        </ul>
+      </div>
     </div>
   );
 };
 
 export default App;
+
+const CombinationRow = ({ n, index, removeSelf }) => {
+  const [checked, setChecked] = React.useState(false);
+  return (
+    <>
+      <li key={n} className="flex flex-row justify-around items-center w-1/2 transition-all">
+        {n.map((num) => {
+          return (
+            <>
+              <div
+                className={`rounded-full w-7 h-7 text-center font-bold ${
+                  index % 2 === 0
+                    ? "border-2 border-black"
+                    : "bg-black text-white"
+                } ${checked ? "opacity-10" : ""}`}
+              >
+                {num}
+              </div>
+            </>
+          );
+        })}
+
+        <button
+          onClick={checked ? removeSelf : () => setChecked((c) => !c)}
+          className="text-lg"
+        >
+          {checked ? "❌" : "✅"}
+        </button>
+        <button
+          onClick={() => setChecked((c) => !c)}
+          className={`${checked ? "" : "hidden"}`}
+        >
+          🔄
+        </button>
+      </li>
+      <hr className="w-1/2"></hr>
+    </>
+  );
+};
